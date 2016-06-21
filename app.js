a2dbde04530a82d2ae88c859d7c7dea016cff4ae
huguet10 <huguet10@gmail.com>
2016-06-21 09:48:43 +0200

    Added controller, model and connection with MongoDB

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
-var express = require("express"),  
+var express = require("express"),
     app = express(),
     bodyParser  = require("body-parser"),
     methodOverride = require("method-override");
     mongoose = require('mongoose');
 
-app.use(bodyParser.urlencoded({ extended: false }));  
-app.use(bodyParser.json());  
-app.use(methodOverride());
+    // Connection to DB
+    mongoose.connect('mongodb://localhost/tvshows', function(err, res) {
+      if(err) throw err;
+      console.log('Connected to Database');
+    });
 
-var router = express.Router();
+    // Middlewares
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+    app.use(methodOverride());
 
-router.get('/', function(req, res) {  
-   res.send("Hello World!");
-});
+    // Import Models and controllers
+    var models     = require('./models/tvshow')(app, mongoose);
+    var TVShowCtrl = require('./controllers/tvshows');
 
-app.use(router);
+    // Example Route
+    var router = express.Router();
+    router.get('/', function(req, res) {
+      res.send("Hello world!");
+    });
+    app.use(router);
 
-app.listen(3000, function() {  
-  console.log("Node server running on http://localhost:3000");
-});
+    // API routes
+    var tvshows = express.Router();
+
+    tvshows.route('/tvshows')
+      .get(TVShowCtrl.findAllTVShows)
+      .post(TVShowCtrl.addTVShow);
+
+    tvshows.route('/tvshows/:id')
+      .get(TVShowCtrl.findById)
+      .put(TVShowCtrl.updateTVShow)
+      .delete(TVShowCtrl.deleteTVShow);
+
+    app.use('/api', tvshows);
+
+    // Start server
+    app.listen(3000, function() {
+      console.log("Node server running on http://localhost:3000");
+    });
